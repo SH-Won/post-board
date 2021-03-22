e41a936a447f3ef9ca23218fa2fb49713c705156
SH-Won <1317good@naver.com>
2021-03-23 02:22:25 +0900

    redeploy

--- server/index.js
+++ server/index.js
@@ -66,5 +66,5 @@ if (process.env.NODE_ENV === "production") {
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-  console.log(`서버 연결! 포트: ${port} 몽고디비 : ${config.mongoURI}`)
+  console.log(`서버 연결! 포트: ${port} 몽고디비 : ${config.mongoURI} success`)
 });
